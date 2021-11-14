58795d6419f3ba331e9845c4320ff7b6107bf616
TADAS DELININKAITIS <t4dam94@gmail.com>
2021-11-14 17:57:24 +0200

    6: Sukurta API funkcija

--- js/Components/ToyGridComponent.js
+++ js/Components/ToyGridComponent.js
@@ -1,9 +1,32 @@
 class ToyGridComponent {
   constructor() {
+    this.state = {
+      loading: false,
+      toys: [],
+    };
     this.init();
   }
+  initFetch = () => API.fetchToys(this.saveToys, alert);
+
+  saveToys = (toys) => {
+    this.state.toys = toys;
+    this.state.loading = false;
+    this.render();
+  };
+
   init = () => {
+    this.state.loading = true;
+    this.initFetch();
     this.htmlElement = document.createElement("div");
-    this.htmlElement.innerHTML = "Some toys, yall";
+    this.render();
+  };
+
+  render = () => {
+    const { loading } = this.state;
+    if (loading) {
+      this.htmlElement.innerHTML = "siunčiama...";
+    } else {
+      this.htmlElement.innerHTML = "parsiųsta...";
+    }
   };
 }
